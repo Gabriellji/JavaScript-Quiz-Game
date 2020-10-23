2e145445f6086429649752212e1b29c19d42fa07
Gabriellji <gabriellji@yahoo.com>
2020-10-24 00:18:46 +0300

    feat: data for js quiz

---
+++ script.js
@@ -0,0 +1,72 @@
+const quizQuestions = [
+    {
+        question: 'Is javascript and java the same thing?',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'Did Michael Jackson invent Java Script?',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'Can JS be used for front end and back end?',
+        isCorrectAnswer: true,
+        score: 0
+    },
+    {
+        question: 'Can we reassign const variable?',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'document.write(NaN == NaN) ? ',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'Is undefined a data type?',
+        isCorrectAnswer: true,
+        score: 0
+    },
+    {
+        question: '{} === {} ?',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'Is JavaScript case-sensitive?',
+        isCorrectAnswer: true,
+        score: 0
+    },
+    {
+        question: 'The external JavaScript file must contain the <script> tag',
+        isCorrectAnswer: true,
+        score: 0
+    },
+    {
+        question: 'document.write(String("Hello") == "Hello") ? ',
+        isCorrectAnswer: true,
+        score: 0
+    },
+    {
+        question: 'document.write( 10 > 9 > 8 === true ) ? ',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'All keys in an object are strings.',
+        isCorrectAnswer: true,
+        score: 0
+    },
+    {
+        question: 'document.write(( true + false ) > 2 + true ) ? ',
+        isCorrectAnswer: false,
+        score: 0
+    },
+    {
+        question: 'document.write(Number("1") - 1 == 0) ?',
+        isCorrectAnswer: true,
+        score: 0
+    },
+]
