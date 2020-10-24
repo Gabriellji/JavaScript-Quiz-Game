0efa77499b699ec07dbfc58630d35e1163e41bee
Gabriellji <gabriellji@yahoo.com>
2020-10-24 13:31:54 +0300

    feat: test logic for game

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const BUTTONS = document.querySelectorAll('.eightbit-btn');
+
+
+const QUESTIONWRAP = document.querySelector('.question')
+
 const quizQuestions = [
     {
         question: 'Is javascript and java the same thing?',
@@ -69,4 +74,77 @@ const quizQuestions = [
         isCorrectAnswer: true,
         score: 0
     },
-]
+];
+
+let score = 0;
+let currentQuestionIndex = 0;
+
+const returnCurrentQuiz = (array) => {
+    console.log(array[currentQuestionIndex])
+    return array[currentQuestionIndex];
+}
+
+const currentQuestion = returnCurrentQuiz(quizQuestions).question;
+
+const correctAnswer = returnCurrentQuiz(quizQuestions).isCorrectAnswer;
+
+let TRUE_BUTTON = document.querySelector('.true');
+
+let FALSE_BUTTON = document.querySelector('.false');
+
+
+const isCorrectAnswer = (answer) => {
+    // if (answer === correctAnswer) {
+    //     score++;
+    // }
+    // currentQuestionIndex++;
+    answer === correctAnswer;
+}
+
+// const guess = (array, answer) => {
+//     if (returnCurrentQuiz(array).isCorrectAnswer(answer)) {
+//       score++;
+//     }
+//     currentQuestionIndex++;
+//   }
+// QUESTIONWRAP.innerHTML = quizQuestions[0].question;
+// index = 0;
+// function changeText() {
+//     index++;
+//     index %= quizQuestions.length
+//     QUESTIONWRAP.innerHTML = quizQuestions[index].question;
+// }
+
+//console.log(getRandomQuiz(quizQuestions).question)
+
+BUTTONS.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        let flag;
+
+        if(e.target.classList.contains('true')) {
+            console.log('in true if');
+            flag = true;
+            isCorrectAnswer(flag);
+            returnCurrentQuiz(quizQuestions).question
+            currentQuestionIndex++;
+            score += 10;
+            console.log(score)
+        } else {
+            console.log('in false if');
+            flag = false;
+            isCorrectAnswer(flag);
+            returnCurrentQuiz(quizQuestions).question
+            currentQuestionIndex++;
+        }
+        QUESTIONWRAP.innerHTML = returnCurrentQuiz(quizQuestions).question;
+        // finish++;
+        // changeText();
+        // console.log(finish)
+        // if (finish === 14) {
+        //     alert('hoho')
+        // }
+        //setQuiz(QUESTIONWRAP, quizQuestions, 'question')
+    })
+})
+
+console.log(quizQuestions[0]['question'])
