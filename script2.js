1b64fb9368c0be2888433d73a03a217bfea127bb
xuleasca <florea.roxana5@yahoo.com>
2020-10-29 11:53:11 +0000

    added js

---
+++ script2.js
@@ -0,0 +1,9 @@
+
+let pics = document.querySelectorAll(".pic");
+let characters = document.querySelector('.characters');
+
+
+     characters.addEventListener('click', function(){
+          console.log('hello')
+      characters.style.display = 'none'
+     })
