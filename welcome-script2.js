aeb76129a330031ff70be7cf83f1815be1d5d837
xuleasca <florea.roxana5@yahoo.com>
2020-11-01 20:50:49 +0000

    added speech bubble

--- welcome-script2.js
+++ welcome-script2.js
@@ -1,42 +1,39 @@
-
 let pics = document.querySelectorAll(".pic");
 let characters = document.querySelector('.characters');
-let wrapper = document.querySelector('.main_panel')
-let button = document.querySelector('.start-btn')
+let wrapper = document.querySelector('.main_panel');
+let button = document.querySelector('.start-btn');
+let bubble = document.querySelector('.bubble')
 
-let roxana = document.querySelector('.roxana')
-let jane = document.querySelector('.jane')
-let nadine = document.querySelector('.nadine')
-let viet = document.querySelector('.viet')
+let roxana = document.querySelector('.roxana');
+let jane = document.querySelector('.jane');
+let nadine = document.querySelector('.nadine');
+let viet = document.querySelector('.viet');
 
-  roxana.addEventListener('click', function(){
-      wrapper.style.display = 'none';
-      document.querySelector('.card-container-roxana').style.display = 'block';
-      button.style.display = 'block';
-  })
+roxana.addEventListener('click', function () {
+    wrapper.style.display = 'none';
+    document.querySelector('.card-container-roxana').style.display = 'block';
+    button.style.display = 'block';
+    bubble.style.display = 'block';
 
-  jane.addEventListener('click', function(){
+})
+
+jane.addEventListener('click', function () {
     wrapper.style.display = 'none';
     document.querySelector('.card-container-jane').style.display = 'block';
     button.style.display = 'block';
+    bubble.style.display = 'block';
 })
 
-nadine.addEventListener('click', function(){
+nadine.addEventListener('click', function () {
     wrapper.style.display = 'none';
     document.querySelector('.card-container-nadine').style.display = 'block';
     button.style.display = 'block';
+    bubble.style.display = 'block';
 })
-    
-viet.addEventListener('click', function(){
+
+viet.addEventListener('click', function () {
     wrapper.style.display = 'none';
     document.querySelector('.card-container-viet').style.display = 'block';
     button.style.display = 'block';
+    bubble.style.display = 'block';
 })
-    
-
-
-
-
-
-
-
