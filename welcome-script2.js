9a6a9f3f9d53c2f3e127187482e3d85f346bbe11
xuleasca <florea.roxana5@yahoo.com>
2020-11-01 16:48:31 +0000

    made pages change dynamically

--- welcome-script2.js
+++ welcome-script2.js
@@ -1,24 +1,42 @@
-console.log('hello')
-let pics = document.querySelectorAll("img");
+
+let pics = document.querySelectorAll(".pic");
 let characters = document.querySelector('.characters');
+let wrapper = document.querySelector('.main_panel')
+let button = document.querySelector('.start-btn')
 
 let roxana = document.querySelector('.roxana')
 let jane = document.querySelector('.jane')
 let nadine = document.querySelector('.nadine')
 let viet = document.querySelector('.viet')
 
- roxana.addEventListener('click', function(){
-   window.location.href='./index.roxana.html'
- })
+  roxana.addEventListener('click', function(){
+      wrapper.style.display = 'none';
+      document.querySelector('.card-container-roxana').style.display = 'block';
+      button.style.display = 'block';
+  })
 
- jane.addEventListener('click', function(){
-   window.location.href='./index.jane.html'
- })
+  jane.addEventListener('click', function(){
+    wrapper.style.display = 'none';
+    document.querySelector('.card-container-jane').style.display = 'block';
+    button.style.display = 'block';
+})
 
- nadine.addEventListener('click', function(){
-   window.location.href='./index.nadine.html'
- })
-  
- viet.addEventListener('click', function(){
-   window.location.href='./index.viet.html'
- })
+nadine.addEventListener('click', function(){
+    wrapper.style.display = 'none';
+    document.querySelector('.card-container-nadine').style.display = 'block';
+    button.style.display = 'block';
+})
+    
+viet.addEventListener('click', function(){
+    wrapper.style.display = 'none';
+    document.querySelector('.card-container-viet').style.display = 'block';
+    button.style.display = 'block';
+})
+    
+
+
+
+
+
+
+
